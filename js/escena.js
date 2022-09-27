eab4d990eb6c9817a9663670aa9afa0599c7d3c5
karolz123 <320karol.52@gmail.com>
2022-09-27 12:50:58 -0500

    second commit

--- js/escena.js
+++ js/escena.js
@@ -1,52 +1,62 @@
-//escenario
-const scene = new THREE.Scene()
-scene.background = new THREE.Color(0x008000)
+//ESCENARIO
 
-//fondo
-let loader = new THREE.TextureLoader();
-loader.load('../imagenes/luna.jpg', function(texture){
- scene.background = texture;
+let scene;
+
+function init(){
+const scene = new THREE.Scene();
+var loader = new THREE.TextureLoader();
+
+loader.load("../IMG/paisajeinterior.jpg", function(texture){
+scene.background = texture;
 });
 
+//CAMARA
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1 );
 
-//camara
-const camara = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//RENDER
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
+const luz = new THREE.DirectionalLight(0xffffff, 6);
+luz.position.set(4, 8, 4);
+scene.add(luz)
 
-//render
-const render = new THREE.WebGLRenderer();
-render.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( render.domElement );
+//3D
+const loader2 = new THREE.GLTFLoader();
+loader2.load("../IMG/TROLL/scene.gltf", function(gltf){
+    troll = gltf.scene.children[0];
+    troll.scale.set(0.2, 0.2, 0.2);
+    scene.add(gltf.scene)
+    renderer.render(scene, camera);
+    troll.position.y = -10
+    troll.position.x = 0
+})
 
-//GEOMETRIAS 
-const geometry = new THREE.CylinderGeometry( 3, 3, 15, 15 );
-const textureLoader= new THREE.TextureLoader();
-const matcap= textureLoader.load('../imagenes/luna.jpg')
+const loader3 = new THREE.GLTFLoader();
+loader3.load("../IMG/Anciana1/scene.gltf", function(gltf){
+    anciana = gltf.scene.children[0];
+    anciana.scale.set(0.7, 0.7, 0.7);
+    scene.add(gltf.scene)
+    renderer.render(scene, camera);
+    anciana.position.y = -1
+    anciana.position.x = -10
+})
 
-const material13 = new THREE.MeshMatcapMaterial()
-material13.matcap = matcap
-material13.flatShading = true
+//CONTROLADORES
+var control = new THREE.OrbitControls(camera, renderer.domElement)
+control.maxDistance = 40
+control.minDistance = 5
 
-const cube = new THREE.Mesh(geometry,material13)
-scene.add(cube);
-camara.position.z = 40;
-camara.position.x = 1;
-camara.position.y = 1;
+camera.position.z = 20;
 
-//edges
-const edges = new THREE.EdgesGeometry( geometry );
-const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0x663300 } ) );
-scene.add( line );
-
-//animación 
-function animate(){
-    requestAnimationFrame( animate );
-    cube.rotation.y += 0.01;
-    cube.rotation.x += 0.01;
-    line.rotation.x += 0.01;
-    line.rotation.y += 0.01;
-    render.render( scene, camara );
+//FUNCION
+function animate() {
+requestAnimationFrame( animate );
+renderer.render( scene, camera, );
+//flycontrols.update(0.5)
 }
 animate();
+}
 
-//webgl libreria que permite ver los archivos guardados en pantalla 
+init();
